3a7e5c573dac2996a5ff370755f604a7163eb568
Cookie Engineer <cookiengineer@protonmail.ch>
2025-05-31 14:17:24 +0200

    :rocket: System API

--- source/serve.go
+++ source/serve.go
@@ -71,7 +71,13 @@ func main() {
 
 		if request.Method == http.MethodGet {
 
-			payload, err := json.MarshalIndent(cache.Systems, "", "\t")
+			identifiers := make([]string, 0)
+
+			for id, _ := range cache.Systems {
+				identifiers = append(identifiers, id)
+			}
+
+			payload, err := json.MarshalIndent(identifiers, "", "\t")
 
 			if err == nil {
 
@@ -88,6 +94,43 @@ func main() {
 				response.Header().Set("Content-Type", "application/json")
 				response.WriteHeader(http.StatusInternalServerError)
 				response.Write([]byte("[]"))
+
+			}
+
+		} else {
+
+			response.Header().Set("Content-Type", "application/json")
+			response.WriteHeader(http.StatusMethodNotAllowed)
+			response.Write([]byte("[]"))
+
+		}
+
+	})
+
+	http.HandleFunc("/api/systems/{name}", func(response http.ResponseWriter, request *http.Request) {
+
+		if request.Method == http.MethodGet {
+
+			name := request.PathValue("name")
+			system := cache.GetSystem(name)
+
+			if system != nil {
+
+				payload, _ := json.MarshalIndent(system, "", "\t")
+
+				fmt.Println("> GET /api/systems/" + name + ": ok")
+
+				response.Header().Set("Content-Type", "application/json")
+				response.WriteHeader(http.StatusOK)
+				response.Write(payload)
+
+			} else {
+
+				fmt.Println("> GET /api/systems/" + name + ": error")
+
+				response.Header().Set("Content-Type", "application/json")
+				response.WriteHeader(http.StatusNotFound)
+				response.Write([]byte("{}"))
 
 			}
 
