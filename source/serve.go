a98e7882c5bfee65e0d7791747412b640a0469e7
Cookie Engineer <cookiengineer@protonmail.ch>
2025-05-31 14:44:48 +0200

    :rocket: HandleSystems/HandleVulnerabilities

--- source/serve.go
+++ source/serve.go
@@ -67,82 +67,8 @@ func main() {
 
 	http.Handle("/", fsrv)
 
-	http.HandleFunc("/api/systems", func(response http.ResponseWriter, request *http.Request) {
-
-		if request.Method == http.MethodGet {
-
-			identifiers := make([]string, 0)
-
-			for id, _ := range cache.Systems {
-				identifiers = append(identifiers, id)
-			}
-
-			payload, err := json.MarshalIndent(identifiers, "", "\t")
-
-			if err == nil {
-
-				fmt.Println("> GET /api/systems: ok")
-
-				response.Header().Set("Content-Type", "application/json")
-				response.WriteHeader(http.StatusOK)
-				response.Write(payload)
-
-			} else {
-
-				fmt.Println("> GET /api/systems: error")
-
-				response.Header().Set("Content-Type", "application/json")
-				response.WriteHeader(http.StatusInternalServerError)
-				response.Write([]byte("[]"))
-
-			}
-
-		} else {
-
-			response.Header().Set("Content-Type", "application/json")
-			response.WriteHeader(http.StatusMethodNotAllowed)
-			response.Write([]byte("[]"))
-
-		}
-
-	})
-
-	http.HandleFunc("/api/systems/{name}", func(response http.ResponseWriter, request *http.Request) {
-
-		if request.Method == http.MethodGet {
-
-			name := request.PathValue("name")
-			system := cache.GetSystem(name)
-
-			if system != nil {
-
-				payload, _ := json.MarshalIndent(system, "", "\t")
-
-				fmt.Println("> GET /api/systems/" + name + ": ok")
-
-				response.Header().Set("Content-Type", "application/json")
-				response.WriteHeader(http.StatusOK)
-				response.Write(payload)
-
-			} else {
-
-				fmt.Println("> GET /api/systems/" + name + ": error")
-
-				response.Header().Set("Content-Type", "application/json")
-				response.WriteHeader(http.StatusNotFound)
-				response.Write([]byte("{}"))
-
-			}
-
-		} else {
-
-			response.Header().Set("Content-Type", "application/json")
-			response.WriteHeader(http.StatusMethodNotAllowed)
-			response.Write([]byte("[]"))
-
-		}
-
-	})
+	server.HandleSystems(cache)
+	server.HandleVulnerabilities(cache)
 
 	fmt.Println("Listening on http://localhost:3000")
 
